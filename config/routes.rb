27601c6de9380cfa23d9a0ce6ae747b496f13d95
raquelhosein <raquelhosein12@gmail.com>
2015-11-06 10:15:27 -0500

    merger with routes and controller fix

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ get '/sign_in' => 'users#new'
 get '/users/login' => 'users#login'
 get '/users/profile' => 'users#profile', as: :user_profile
 
+
+get '/lights/new' => 'lights#new'
+get '/darks/new' => 'darks#new'
+
+
 post '/sessions' => 'sessions#create'
   delete '/sessions' => 'sessions#destroy', as: :log_out
 
@@ -15,6 +20,6 @@ post '/sessions' => 'sessions#create'
 get '/questions/people' => 'questions#people'
 get '/questions/starships' => 'questions#starships'
 get '/questions/planets' => 'questions#planets'
-
+get 'questions/species' => 'questions#species'
 
 end
