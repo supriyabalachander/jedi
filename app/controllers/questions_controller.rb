24194b20d01c88d97c2cb9d839ac2e0bd350e8de
Philip Welber <prwelber@gmail.com>
2015-11-06 07:47:17 -0500

    almost finished with all api calls

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,17 +2,16 @@ class QuestionsController < ApplicationController
 
   def people 
     
+    # takes random number from range indicated on API documentation and concatenates to end of api call below
     num1 = rand(1..87).to_s
     num2 = rand(1..87).to_s
     puts num2
     num3 = rand(1..87).to_s
     puts num3
-    # num4 = rand(1..87).to_s
     @answer = HTTParty.get('http://swapi.co/api/people/' + num1)
     puts @answer
     @answer2 = HTTParty.get('http://swapi.co/api/people/' + num2)
     @answer3 = HTTParty.get('http://swapi.co/api/people/' + num3)
-    # @answer4 = HTTParty.get('http://swapi.co/api/people/' + num4)
 
   end
 
@@ -23,12 +22,10 @@ class QuestionsController < ApplicationController
     puts num2
     num3 = rand(1..37).to_s
     puts num3
-    # num4 = rand(1..87).to_s
     @answer = HTTParty.get('http://swapi.co/api/starships/' + num1)
     puts @answer
     @answer2 = HTTParty.get('http://swapi.co/api/starships/' + num2)
     @answer3 = HTTParty.get('http://swapi.co/api/starships/' + num3)
-    # @answer4 = HTTParty.get('http://swapi.co/api/people/' + num4)
 
   end
 
@@ -39,12 +36,24 @@ class QuestionsController < ApplicationController
     puts num2
     num3 = rand(1..61).to_s
     puts num3
-    # num4 = rand(1..87).to_s
     @answer = HTTParty.get('http://swapi.co/api/planets/' + num1)
     puts @answer
     @answer2 = HTTParty.get('http://swapi.co/api/planets/' + num2)
     @answer3 = HTTParty.get('http://swapi.co/api/planets/' + num3)
-    # @answer4 = HTTParty.get('http://swapi.co/api/people/' + num4)
+
+  end
+
+  def species
+
+    num1 = rand(1..37.to_s)
+    num2 = rand(1..37.to_s)
+    num3 = rand(1..37.to_s)
+
+    @answer = HTTParty.get('http://swapi.co/api/species/' + num1)
+    puts @answer
+    @answer2 = HTTParty.get('http://swapi.co/api/species/' + num2)
+    @answer3 = HTTParty.get('http://swapi.co/api/species/' + num3)
+
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,15 @@ Rails.application.routes.draw do
 
 root 'users#index'
 
+
 get '/sign_in' => 'users#new'
 
+
+# routes for trivia questions - 4 categories
 get '/questions/people' => 'questions#people'
 get '/questions/starships' => 'questions#starships'
 get '/questions/planets' => 'questions#planets'
+get '/questions/species' => 'questions#species'
 
 
 end
